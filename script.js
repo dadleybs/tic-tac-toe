d520409e77529f47204adb039117ea60956fa9b2
dadleybs <bradley.smoak@gmail.com>
2017-01-11 23:27:55 -0600

    It's ALIVE!

--- script.js
+++ script.js
@@ -1,59 +1,56 @@
 // JavaScript document
-var board = ['#a1', '#a2', '#a3', '#b1', '#b2', '#b3', '#c1', '#c2', '#c3'];
-var $1 = $(board[0]);
-var $2 = $(board[1]);
-var $3 = $(board[2]);
-var $4 = $(board[3]);
-var $5 = $(board[4]);
-var $6 = $(board[5]);
-var $7 = $(board[6]);
-var $8 = $(board[7]);
-var $9 = $(board[8]);
-var whoseTurn = true;
+// Play the game
+$(document).ready(function() {	
+	$('#again').hide();
+	var board = ['#a1', '#a2', '#a3', '#b1', '#b2', '#b3', '#c1', '#c2', '#c3'];
+	var $1 = $(board[0]);
+	var $2 = $(board[1]);
+	var $3 = $(board[2]);
+	var $4 = $(board[3]);
+	var $5 = $(board[4]);
+	var $6 = $(board[5]);
+	var $7 = $(board[6]);
+	var $8 = $(board[7]);
+	var $9 = $(board[8]);
+	var whoseTurn = true;
 
-function makeClicked() {
-		$('.box').click(function() {
-			$(this).addClass('clicked');
-		})
+	function makeClicked(box) {
+		box.addClass('clicked');
 	};
-function checkClicked(box) {
-	if (!box.hasClass('clicked')) {
-	makeClicked();
-	return true;
-	} else {
-	alert("Sorry, that space has already been claimed. Try another one!");
-	return false;
+	function checkClicked(box) {
+		if (!box.hasClass('clicked')) {
+		makeClicked(box);
+		return true;
+		} else {
+		alert("Sorry, that space has already been claimed. Try another one!");
+		return false;
+		}
 	}
-}
-function turnRed() {
-		$('.box').click(function() {
-			$(this).addClass('red');
-		})
+	function turnRed(thisElement) {
+		thisElement.addClass('red');
 	};
-function turnBlue() {
-		$('.box').click(function() {
-			$(this).addClass('blue');
-		})
+	function turnBlue(thatElement) {
+		thatElement.addClass('blue');
 	};
-function checkWin() {
-	var red1 = $1.hasClass('red');
-	var red2 = $2.hasClass('red');
-	var red3 = $3.hasClass('red');
-	var red4 = $4.hasClass('red');
-	var red5 = $5.hasClass('red');
-	var red6 = $6.hasClass('red');
-	var red7 = $7.hasClass('red');
-	var red8 = $8.hasClass('red');
-	var red9 = $9.hasClass('red');
-	var blue1 = $1.hasClass('blue');
-	var blue2 = $2.hasClass('blue');
-	var blue3 = $3.hasClass('blue');
-	var blue4 = $4.hasClass('blue');
-	var blue5 = $5.hasClass('blue');
-	var blue6 = $6.hasClass('blue');
-	var blue7 = $7.hasClass('blue');
-	var blue8 = $8.hasClass('blue');
-	var blue9 = $9.hasClass('blue');
+	function checkWin() {
+		var red1 = $1.hasClass('red');
+		var red2 = $2.hasClass('red');
+		var red3 = $3.hasClass('red');
+		var red4 = $4.hasClass('red');
+		var red5 = $5.hasClass('red');
+		var red6 = $6.hasClass('red');
+		var red7 = $7.hasClass('red');
+		var red8 = $8.hasClass('red');
+		var red9 = $9.hasClass('red');
+		var blue1 = $1.hasClass('blue');
+		var blue2 = $2.hasClass('blue');
+		var blue3 = $3.hasClass('blue');
+		var blue4 = $4.hasClass('blue');
+		var blue5 = $5.hasClass('blue');
+		var blue6 = $6.hasClass('blue');
+		var blue7 = $7.hasClass('blue');
+		var blue8 = $8.hasClass('blue');
+		var blue9 = $9.hasClass('blue');
 	// check for Red win
 	if (red3 && red5 && red7) {
 		alert("Red wins! How about a rematch?");
@@ -114,9 +111,6 @@ function resetBoard() {
 	$('.box').removeClass('red blue clicked');
 	$('#again').hide();
 }
-// Play the game
-$(document).ready(function() {	
-	$('#again').hide();
 	$('.box').click(function() {
 		var box = $(this);
 		if (checkClicked(box)) {
