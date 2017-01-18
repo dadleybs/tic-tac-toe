60fe56c9532df708a04fe5686d84a6a5f83aab41
dadleybs <bradley.smoak@gmail.com>
2017-01-17 20:54:36 -0600

    Where did it all go??

--- script.js
+++ script.js
@@ -30,52 +30,25 @@ $(document).ready(function() {
     thatElement.addClass('blue');
   }
 
-  function isGameWon() {
-    // check for Red win
-    var red1 = $1.hasClass('red');
-    var red2 = $2.hasClass('red');
-    var red3 = $3.hasClass('red');
-    var red4 = $4.hasClass('red');
-    var red5 = $5.hasClass('red');
-    var red6 = $6.hasClass('red');
-    var red7 = $7.hasClass('red');
-    var red8 = $8.hasClass('red');
-    var red9 = $9.hasClass('red');
-    if (red3 && red5 && red7 || 
-      red1 && red2 && red3 || 
-      red1 && red4 && red7 || 
-      red1 && red5 && red9 ||
-      red2 && red5 && red8 ||
-      red3 && red6 && red9 ||
-      red4 && red5 && red6 ||
-      red7 && red8 && red9) {
-      return true;
-    }
-    // check for Blue win
-    var blue1 = $1.hasClass('blue');
-    var blue2 = $2.hasClass('blue');
-    var blue3 = $3.hasClass('blue');
-    var blue4 = $4.hasClass('blue');
-    var blue5 = $5.hasClass('blue');
-    var blue6 = $6.hasClass('blue');
-    var blue7 = $7.hasClass('blue');
-    var blue8 = $8.hasClass('blue');
-    var blue9 = $9.hasClass('blue');
-    if (blue1 && blue2 && blue3 ||
-      blue1 && blue4 && blue7 ||
-      blue1 && blue5 && blue9 ||
-      blue2 && blue5 && blue8 ||
-      blue3 && blue6 && blue9 ||
-      blue4 && blue5 && blue6 ||
-      blue7 && blue8 && blue9 ||
-      blue3 && blue5 && blue7) {
-      return true;
-    }
-    // check for Draw
-    if ($('.clicked').length === 9) {
-      alert("The game is a tie! Try again?");
-      return true;
-    }
+  function hasPlayerWon(player) {
+    var e1 = $1.hasClass(player);
+    var e2 = $2.hasClass(player);
+    var e3 = $3.hasClass(player);
+    var e4 = $4.hasClass(player);
+    var e5 = $5.hasClass(player);
+    var e6 = $6.hasClass(player);
+    var e7 = $7.hasClass(player);
+    var e8 = $8.hasClass(player);
+    var e9 = $9.hasClass(player);
+    return e3 && e5 && e7 || 
+      e1 && e2 && e3 || 
+      e1 && e4 && e7 || 
+      e1 && e5 && e9 ||
+      e2 && e5 && e8 ||
+      e3 && e6 && e9 ||
+      e4 && e5 && e6 ||
+      e7 && e8 && e9;
+  }
 
   function resetBoard() {
     $('.box').removeClass('red blue clicked');
@@ -96,18 +69,16 @@ $(document).ready(function() {
       whoseTurn = !whoseTurn;
     }
 
-    if (isGameWon() && whoseTurn) {
+    if (hasPlayerWon('blue')) {
       alert("Blue wins! How about a rematch?");
-    } else if (isGameWon() && !whoseTurn) {
+      $('#again').show();
+    } else if (hasPlayerWon('red')) {
       alert("Red wins! How about a rematch?");
+      $('#again').show();
     } else if ($('.clicked').length === 9) {
       alert("The game is a tie! Try again?");
-    }
-    
-    if (isGameWon()) {
       $('#again').show();
-    } else {}
-  });
+    }
   
   $('#again').click(function () {
     resetBoard();
